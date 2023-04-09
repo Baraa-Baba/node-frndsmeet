eb1b2caeef0bb1d7c3bb5f8316fc22ec8e6cd34c
Baraa Baba <99536124+Baraa-Baba@users.noreply.github.com>
2023-04-09 21:39:18 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,166 @@
+const express = require("express");
+const http = require("http");
+// const enforce = require("express-sslify");
+const app = express();
+const server = http.createServer(app);
+//const socket = require('socket.io', { rememberTransport: false, transports: ['WebSocket', 'Flash Socket', 'AJAX long-polling'] })
+const socket = require("socket.io");
+const io = socket(server);
+const path = require("path");
+const _ = require("lodash");
+
+
+// app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+app.use(express.static("./client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+let users = [];
+let queue = [];
+
+io.on("connection", (socket) => {
+  let isBusy = false;
+  if (!_.includes(users, socket.id)) {
+    users.push(socket.id);
+  }
+  io.on('connection', (socket) => { 
+    // Handle signaling events
+    socket.on('offer', (offer) => {   
+      io.to(offer.id).emit("offer", offer) 
+    });
+  
+    socket.on('answer', (answer) => { 
+      io.to(answer.id).emit("answer", answer) 
+    });
+  
+    socket.on('candidate', (candidate) => {
+      // Broadcast candidate to all other clients
+      socket.broadcast.emit('candidate', candidate);
+    });
+  
+    socket.on('disconnect', () => { 
+    });
+  });
+  if (!_.includes(users, socket.id)) {
+    users.push(socket.id);
+  }
+  socket.emit("yourID", socket.id);
+  io.sockets.emit("allUsers", users);
+
+  socket.on("disconnect", () => {
+    _.pull(users, socket.id);
+
+    const userInQueue = _.find(queue, u => u.id === socket.id);
+
+    if (userInQueue) {
+      _.remove(queue, { id: userInQueue.id });
+      isBusy = false;
+    }
+  });
+
+  socket.on("leaveQueue", () => {
+    const userInQueue = _.find(queue, u => u.id === socket.id);
+
+    if (userInQueue && isBusy) {
+      isBusy = false;
+      _.remove(queue, { id: userInQueue.id });
+    }
+  });
+
+  socket.on("sendMessage", (data) => {
+    socket.emit("messageSent", {
+      message: data.message,
+    });
+
+    io.to(data.peerId).emit("receiveMessage", {
+      message: data.message,
+    });
+  });
+  socket.on("endCall",(data)=> {
+    io.to(data.peerId).emit("isCallEnded", {
+      message: data.message,
+    });
+  })
+  socket.on("sendIsInverted", (data) => {
+    io.to(data.peerId).emit("isInverted", {
+      message: data.message,
+    });
+  }
+  );
+  socket.on("findPartner", (data) => {
+    let uAchievedThierPrefrence 
+    let dataAchievedThierPrefrence
+    let uAchievedThierCountryPrefrence
+    let datadataAchievedThierPrefrence
+    viablePartner = _.find(queue, u => {  
+      if(u.id !== socket.id && u.onlyChat === data.onlyChat){
+         uAchievedThierPrefrence = u.userGenderPrefernce === data.userGender
+        || u.userGenderPrefernce=='anygender'
+         dataAchievedThierPrefrence=  data.userGenderPrefernce == u.userGender 
+        || data.userGenderPrefernce=='anygender' 
+        
+        datadataAchievedThierPrefrence=data.userCountryPrefrence == u.userCountry 
+        || data.userCountryPrefrence=='anycountry' 
+
+        uAchievedThierCountryPrefrence= u.userCountryPrefrence === data.userCountry
+        || u.userCountryPrefrence=='anycountry'
+        if(uAchievedThierPrefrence&&dataAchievedThierPrefrence&&datadataAchievedThierPrefrence
+          &&uAchievedThierCountryPrefrence){
+          return true
+        }
+      } 
+       
+     
+    });
+
+    if (!viablePartner && !isBusy) {
+      isBusy = true;
+      const userInQueue = _.find(queue, u => u.id === socket.id);
+      if (!userInQueue) {
+        queue.push({ id: socket.id, onlyChat: data.onlyChat,userGender:data.userGender,prevUser:data.prevUser,
+        userGenderPrefernce:data.userGenderPrefernce,userCountryPrefrence:data.userCountryPrefrence,
+        userCountry:data.userCountry});
+      }
+    } else if (!isBusy) {
+      isBusy = true; 
+      _.remove(queue, { id: viablePartner.id }); 
+      io.to(viablePartner.id).emit("peer", {
+        peerId: socket.id, 
+        initiator: true,
+        userGender:data.userGender,
+        userCountry:data.userCountry,
+      });
+
+      socket.emit("peer", {
+        peerId: viablePartner.id, 
+        initiator: false,
+        userGender:viablePartner.userGender,
+        userCountry:viablePartner.userCountry,
+      });
+    }
+  }); 
+  socket.on("signal", (data) => {
+    if (!data.peerId) {
+      return;
+    }
+
+    isBusy = false;
+    io.to(data.peerId).emit("signal", {
+      signal: data.signal,
+      peerId: socket.id,
+    });
+  });
+
+  // socket.on("close", (data) => {
+  //   io.to(data.peerId).emit("close");
+  // });
+});
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
